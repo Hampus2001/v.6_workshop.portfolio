b253e54131948e5400cd93ddc9b5f482d8ffdf25
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-03-13 14:00:53 +0100

    reacreated footer after accidentally deleting it :)

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { FaGithub, FaHome } from "react-icons/fa";
+import { LuSun, LuMoon } from "react-icons/lu";
+import { HandleThemeContext } from "@/contexts/ThemeContext";
+import { useContext } from "react";
+
+export default function Footer() {
+  const { theme, setTheme } = useContext(HandleThemeContext);
+
+  const iconStyling =
+    "text-xl sm:text-4xl hover:cursor-pointer hover:text-primary";
+
+  const darkThemeCard =
+    "font-bold text-md bg-gray-900 text-gray-300  flex justify-end items-center p-5 sm:p-6  lg:text-xl";
+  const lightThemeCard =
+    "font-bold text-md bg-neutral-300 text-base-content flex justify-end items-center p-5 sm:p-6  lg:text-xl";
+  return (
+    <div className={theme == "dark" ? darkThemeCard : lightThemeCard}>
+      <div className="flex gap-5 items-center">
+        <Link href="/admin">
+          <p className="hover:text-primary">Admin</p>
+        </Link>
+        {theme == "dark" && (
+          <LuSun className={iconStyling} onClick={() => setTheme("light")} />
+        )}
+        {theme == "light" && (
+          <LuMoon className={iconStyling} onClick={() => setTheme("dark")} />
+        )}
+        <a href="https://github.com/Hampus2001" className={iconStyling}>
+          <FaGithub />
+        </a>
+      </div>
+    </div>
+  );
+}
