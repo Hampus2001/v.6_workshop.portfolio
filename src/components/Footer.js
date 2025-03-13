9c239c21e62cf86234042bee2eaa08efa744aee3
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-03-13 13:28:24 +0100

    test3

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <div className="flex justify-between bg-base-300 font-bold text-xs h-4 items-center p-10 lg:text-xl">
+      <div className="flex gap-5 items-center">
+        <Link href="/">
+          <p>Home</p>
+        </Link>
+        <p>About</p>
+        <p>Contact</p>
+        <Link href="/admin">
+          <p>Admin</p>
+        </Link>
+        <a href="https://github.com/Hampus2001" className="text-xl sm:text-3xl">
+          <FaGithub />
+        </a>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { FaGithub, FaHome } from "react-icons/fa";
+import { LuSun, LuMoon } from "react-icons/lu";
+import { HandleThemeContext } from "@/contexts/ThemeContext";
+import { useContext } from "react";
+
+export default function NavBar() {
+  const { theme, setTheme } = useContext(HandleThemeContext);
+
+  const iconStyling =
+    "text-xl sm:text-4xl hover:cursor-pointer hover:text-primary";
+
+  return (
+    <div className="font-bold text-md bg-base-300 flex justify-between items-center p-5 sm:p-6  lg:text-xl">
+      <div>
+        <Link href="/">
+          <FaHome className={iconStyling} />
+        </Link>
+      </div>
+      <div className="flex gap-5 items-center">
+        <Link href="/about">
+          <p className="hover:text-primary">About</p>
+        </Link>
+        <Link href="/contact">
+          <p className="hover:text-primary">Contact</p>
+        </Link>
+        {theme == "dark" && (
+          <LuSun className={iconStyling} onClick={() => setTheme("light")} />
+        )}
+        {theme == "light" && (
+          <LuMoon className={iconStyling} onClick={() => setTheme("dark")} />
+        )}
+        <a href="https://github.com/Hampus2001" className={iconStyling}>
+          <FaGithub />
+        </a>
+      </div>
+    </div>
+  );
+}
